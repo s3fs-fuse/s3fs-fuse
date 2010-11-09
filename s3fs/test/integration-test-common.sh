ac294bc8856101fd88dffc204538f8bedb85710d
apetresc <apetresc@df820570-a93a-0410-bd06-b72b767a4274>
2010-11-09 07:13:30 +0000

    Making the test bucket be based on ${USER}. This requires the bucket to already exist. S3FS doesn't report an error when mounting if it doesn't exist, so the test fails strangely in such cases.
    The correct way to solve this is to consistently faily when mounting. Also offer a flag to forcefully create the bucket if it doesn't yet exist.
    
    
    git-svn-id: http://s3fs.googlecode.com/svn/trunk@231 df820570-a93a-0410-bd06-b72b767a4274

--- s3fs/test/integration-test-common.sh
+++ s3fs/test/integration-test-common.sh
@@ -4,7 +4,7 @@ S3FS=../src/s3fs
 
 S3FS_CREDENTIALS_FILE=~/.passwd-s3fs
 
-TEST_BUCKET_1=apetresc-s3fs
+TEST_BUCKET_1=${USER}-s3fs-integration-test
 TEST_BUCKET_MOUNT_POINT_1=/mnt/s3fs-test
 
 if [ ! -f "$S3FS_CREDENTIALS_FILE" ]
