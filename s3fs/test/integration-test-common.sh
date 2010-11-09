ec822185dab516f184980993a7f9ddcc6407640e
apetresc <apetresc@df820570-a93a-0410-bd06-b72b767a4274>
2010-11-09 07:22:30 +0000

    Usability improvement: use the ${SUDO_USER} environment provided by sudo to create the bucket based on the user who invoked the test using sudo. Much better this way.
    git-svn-id: http://s3fs.googlecode.com/svn/trunk@232 df820570-a93a-0410-bd06-b72b767a4274

--- s3fs/test/integration-test-common.sh
+++ s3fs/test/integration-test-common.sh
@@ -4,8 +4,8 @@ S3FS=../src/s3fs
 
 S3FS_CREDENTIALS_FILE=~/.passwd-s3fs
 
-TEST_BUCKET_1=${USER}-s3fs-integration-test
-TEST_BUCKET_MOUNT_POINT_1=/mnt/s3fs-test
+TEST_BUCKET_1=${SUDO_USER}-s3fs-integration-test
+TEST_BUCKET_MOUNT_POINT_1=/mnt/${TEST_BUCKET_1}
 
 if [ ! -f "$S3FS_CREDENTIALS_FILE" ]
 then
