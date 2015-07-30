fcb55c2109024ea5c44e35410be21e7371fa3cd0
Robb Kistler <code@robbkistler.com>
2015-07-29 21:18:54 -0700

    Fix mem leaks in openssl_auth.cpp, nss_auth.cpp
    Fix memory leaks in openssl_auth.cpp:s3fs_sha256hexsum and
    nss_auth.cpp:s3fs_sha256hexsum.  Leaks occur every time a file
    is created.

--- src/nss_auth.cpp
+++ src/nss_auth.cpp
@@ -263,6 +263,7 @@ unsigned char* s3fs_sha256hexsum(int fd, off_t start, ssize_t size)
     }else if(-1 == bytes){
       // error
       DPRNNN("file read error(%d)", errno);
+      PK11_DestroyContext(sha256ctx, PR_TRUE);
       return NULL;
     }
     PK11_DigestOp(sha256ctx, buf, bytes);
