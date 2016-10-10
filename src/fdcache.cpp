716baada22f23e123cecf1ce031200e36b860183
Takeshi Nakatani <ggtakec@gmail.com>
2016-10-10 12:16:09 +0000

    Testing patch codes for issue#435

--- src/fdcache.cpp
+++ src/fdcache.cpp
@@ -1963,8 +1963,23 @@ FdEntity* FdManager::Open(const char* path, headers_t* pmeta, ssize_t size, time
   }
   AutoLock auto_lock(&FdManager::fd_manager_lock);
 
+  // search in mapping by key(path)
   fdent_map_t::iterator iter = fent.find(string(path));
-  FdEntity*             ent;
+
+  if(fent.end() == iter && !FdManager::IsCacheDir()){
+    // If the cache directory is not specified, s3fs opens a temporary file
+    // when the file is opened.
+    // Then if it could not find a entity in map for the file, s3fs should
+    // search a entity in all which opened the temporary file.
+    //
+    for(iter = fent.begin(); iter != fent.end(); ++iter){
+      if((*iter).second && (*iter).second->IsOpen()){
+        break;      // found opened fd in mapping
+      }
+    }
+  }
+
+  FdEntity* ent;
   if(fent.end() != iter){
     // found
     ent = (*iter).second;
