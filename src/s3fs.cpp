0863672e2701e524fd6da8c6b13f628aa48c4bd8
Tristen Horton <tristen@tristenhorton.com>
2018-03-13 14:37:34 -0700

    add a more helpful error message for when there are multiple entries for the same bucket in the passwd file

--- src/s3fs.cpp
+++ src/s3fs.cpp
@@ -3925,7 +3925,7 @@ static int parse_passwd_file(bucketkvmap_t& resmap)
       secret    = trim(iter->substr(first_pos + 1, string::npos));
     }
     if(resmap.end() != resmap.find(bucket)){
-      S3FS_PRN_EXIT("same bucket(%s) passwd setting found in passwd file.", ("" == bucket ? "default" : bucket.c_str()));
+      S3FS_PRN_EXIT("there are mutliple entries for the same bucket(%s) in the passwd file.", ("" == bucket ? "default" : bucket.c_str()));
       return -1;
     }
     kv.clear();
