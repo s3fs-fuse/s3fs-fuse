8fb70c5e4aab1d3c2496ff0d5d13520b632ed962
Andrew Gaul <andrew@gaul.org>
2020-09-15 22:11:14 +0900

    Fix expandtab settings for tests
    This matches the main source.

--- test/ut_test.py
+++ test/ut_test.py
@@ -103,6 +103,6 @@ if __name__ == '__main__':
 # tab-width: 4
 # c-basic-offset: 4
 # End:
-# vim600: noet sw=4 ts=4 fdm=marker
-# vim<600: noet sw=4 ts=4
+# vim600: expandtab sw=4 ts=4 fdm=marker
+# vim<600: expandtab sw=4 ts=4
 # 
