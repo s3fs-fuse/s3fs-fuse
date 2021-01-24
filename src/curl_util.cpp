dc9255bc5f3c72c5e75f44c86370d04d6863c505
Andrew Gaul <andrew@gaul.org>
2021-01-25 08:15:17 +0900

    Prefer static_cast where possible (#1531)

--- src/curl_util.cpp
+++ src/curl_util.cpp
@@ -87,7 +87,7 @@ struct curl_slist* curl_slist_sort_insert(struct curl_slist* list, const char* k
     }
 
     struct curl_slist* new_item;
-    if(NULL == (new_item = reinterpret_cast<struct curl_slist*>(malloc(sizeof(*new_item))))){
+    if(NULL == (new_item = static_cast<struct curl_slist*>(malloc(sizeof(*new_item))))){
         free(data);
         return list;
     }
