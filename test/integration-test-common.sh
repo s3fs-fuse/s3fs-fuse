8bf6dce0914693fb952680d8102ddd2c31a3d2a9
Andrew Gaul <andrew@gaul.org>
2022-01-13 08:08:30 +0900

    Call curl --fail to propagate exit code

--- test/integration-test-common.sh
+++ test/integration-test-common.sh
@@ -136,7 +136,7 @@ function start_s3proxy {
     then
         if [ ! -e "${S3PROXY_BINARY}" ]; then
             curl "https://github.com/gaul/s3proxy/releases/download/s3proxy-${S3PROXY_VERSION}/s3proxy" \
-                --location --silent --output "${S3PROXY_BINARY}"
+                --fail --location --silent --output "${S3PROXY_BINARY}"
             chmod +x "${S3PROXY_BINARY}"
         fi
 
@@ -155,7 +155,7 @@ function start_s3proxy {
     if [ -n "${CHAOS_HTTP_PROXY}" ]; then
         if [ ! -e "${CHAOS_HTTP_PROXY_BINARY}" ]; then
             curl "https://github.com/bouncestorage/chaos-http-proxy/releases/download/chaos-http-proxy-${CHAOS_HTTP_PROXY_VERSION}/chaos-http-proxy" \
-                --location --silent --output "${CHAOS_HTTP_PROXY_BINARY}"
+                --fail --location --silent --output "${CHAOS_HTTP_PROXY_BINARY}"
             chmod +x "${CHAOS_HTTP_PROXY_BINARY}"
         fi
 
