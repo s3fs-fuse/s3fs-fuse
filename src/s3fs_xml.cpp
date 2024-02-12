67442cf0543caf66f9b87ae9b58bc1eab4a2d27f
Takeshi Nakatani <ggtakec@gmail.com>
2024-02-12 07:41:44 +0000

    Changed the level of messages by the get_base_exp function

--- src/s3fs_xml.cpp
+++ src/s3fs_xml.cpp
@@ -110,7 +110,7 @@ static unique_ptr_xmlChar get_base_exp(xmlDocPtr doc, const char* exp)
         return {nullptr, xmlFree};
     }
     if(xmlXPathNodeSetIsEmpty(marker_xp->nodesetval)){
-        S3FS_PRN_ERR("marker_xp->nodesetval is empty.");
+        S3FS_PRN_INFO("marker_xp->nodesetval is empty.");
         return {nullptr, xmlFree};
     }
     xmlNodeSetPtr nodes  = marker_xp->nodesetval;
