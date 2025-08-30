0448ff460b1b7aac46ab7d348d8afa4b88dda822
Andrew Gaul <andrew@gaul.org>
2025-08-30 16:13:30 +0900

    Add S3 operation performance counters (#2715)
    These can be used to evaluate changes like #2707.  Ideally tests could
    assert how many operations they expect although this will require a
    localhost HTTP server.

--- src/s3fs_global.cpp
+++ src/s3fs_global.cpp
@@ -37,6 +37,17 @@ std::string region                = "us-east-1";
 std::string cipher_suites;
 std::string instance_name;
 
+std::atomic<long long unsigned> num_requests_head_object;
+std::atomic<long long unsigned> num_requests_put_object;
+std::atomic<long long unsigned> num_requests_get_object;
+std::atomic<long long unsigned> num_requests_delete_object;
+std::atomic<long long unsigned> num_requests_list_bucket;
+std::atomic<long long unsigned> num_requests_mpu_initiate;
+std::atomic<long long unsigned> num_requests_mpu_complete;
+std::atomic<long long unsigned> num_requests_mpu_abort;
+std::atomic<long long unsigned> num_requests_mpu_upload_part;
+std::atomic<long long unsigned> num_requests_mpu_copy_part;
+
 /*
 * Local variables:
 * tab-width: 4
