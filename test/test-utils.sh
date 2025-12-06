a6764b6941aa83a3024a38255a8712a8c7bbb4ef
Andrew Gaul <andrew@gaul.org>
2025-12-05 19:21:07 -0800

    Remove macOS GNU sed (#2770)
    This was only used for the --unbuffered flag.

--- test/test-utils.sh
+++ test/test-utils.sh
@@ -71,11 +71,9 @@ if [ "$(uname)" = "Darwin" ]; then
         export STDBUF_BIN=""
     fi
     export TRUNCATE_BIN="gtruncate"
-    export SED_BIN="gsed"
 else
     export STDBUF_BIN="stdbuf"
     export TRUNCATE_BIN="truncate"
-    export SED_BIN="sed"
 fi
 
 # [NOTE]
