aa9157c80c809ef75f48b58a935e58f8b173dc05
Andrew Gaul <andrew@gaul.org>
2026-03-08 08:01:08 -0700

    Update s3fs.cpp

--- src/s3fs.cpp
+++ src/s3fs.cpp
@@ -3859,8 +3859,6 @@ static bool parse_xattr_keyval(const std::string& xattrpair, std::string& key, s
     // parse key and value
     size_t pos;
     std::string tmpval;
-    // find last
-    //one of xattr pair("user.DosStream.com.apple.lastuseddate#PS:$DATA":"AA==") is wrong format.
     if(std::string::npos == (pos = xattrpair.find_last_of(':'))){
         S3FS_PRN_ERR("one of xattr pair(%s) is wrong format.", xattrpair.c_str());
         return false;
