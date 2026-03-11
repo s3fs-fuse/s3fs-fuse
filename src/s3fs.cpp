65b40431e89a6583f07d280ca312edb5fffd0066
dev.siray <dev.siray@gmail.com>
2026-03-11 17:27:19 +0800

    enable idmap just in fuse3

--- src/s3fs.cpp
+++ src/s3fs.cpp
@@ -4551,8 +4551,10 @@ static void* s3fs_init(struct fuse_conn_info* conn)
          conn->want |= FUSE_CAP_ATOMIC_O_TRUNC;
     }
     #endif
+#if FUSE_USE_VERSION >= 30
     // enable ID MAP
     conn->want_ext |= FUSE_CAP_ALLOW_IDMAP;
+#endif
 
 #if FUSE_USE_VERSION < 30
     if(conn->capable & FUSE_CAP_BIG_WRITES){
